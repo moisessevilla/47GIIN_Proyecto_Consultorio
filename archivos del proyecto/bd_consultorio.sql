c70d7ea274b89a61c6ff6182187614c27e8ebdff
moisessevilla <163861638+moisessevilla@users.noreply.github.com>
2025-01-10 18:39:34 +0100

    Update bd_consultorio.sql
    Se actualiza la BD con la nueva tabla de Médicos especialistas.

--- archivos del proyecto/bd_consultorio.sql
+++ archivos del proyecto/bd_consultorio.sql
@@ -4,10 +4,21 @@ CREATE DATABASE IF NOT EXISTS consultorio;
 -- Crear la tabla Paciente
 CREATE TABLE Paciente (
     id_paciente SERIAL PRIMARY KEY,
-    dni VARCHAR(9) UNIQUE, -- Nuevo campo DNI
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    telefono VARCHAR(15),
-    contrasena VARCHAR(100) NOT NULL
+    dni VARCHAR(9) UNIQUE,              -- Campo DNI
+    nombre VARCHAR(100) NOT NULL,       -- Nombre (máx. 9 carácteres)
+    apellido VARCHAR(100) NOT NULL,     -- Apellido
+    email VARCHAR(100) UNIQUE NOT NULL, -- Email único
+    telefono VARCHAR(9),                -- Teléfono (máx. 9 carácteres)
+    contrasena VARCHAR(100) NOT NULL    -- Contraseña
 );
+
+-- Crear la tabla Médico
+CREATE TABLE Medico (
+    id_medico SERIAL PRIMARY KEY,           -- Identificador único
+    ncolegiado VARCHAR(8) UNIQUE NOT NULL,  -- Número de colegiado (máx. 8 carácteres)
+    nombre VARCHAR(100) NOT NULL,           -- Nombre completo
+    especialidad VARCHAR(50) NOT NULL,      -- Especialidad médica
+    email VARCHAR(100) UNIQUE NOT NULL      -- Email único
+);
+
+
